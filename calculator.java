a39f39af9d1428d9d343db1068a2b41e4b268478
Vingus Allahbux Chhutto <152180739+VingusAllahbux@users.noreply.github.com>
2024-06-22 12:18:13 +0500

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,10 @@
+public class calculator{
+    public static void main(String[] args){
+    int a=100;
+    float b=2.5f;
+    System.out.println(a+b);
+    System.out.println(a-b);
+    System.out.println(a*b);
+    System.out.println(a/b);
+    }
+}
